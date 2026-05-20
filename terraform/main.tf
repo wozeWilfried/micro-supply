51331d2deac439b63f0576edd621492864ef57d8
Woze Wilfried <wilfriedwoze@gmail.com>
2026-05-21 01:29:13 +0200

    fix: changement de nom du SG pour eviter la duplication

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Terraform va créer lui-même la clé manquante sur AWS
+# Clé SSH gérée automatiquement
 resource "aws_key_pair" "digitrans_key" {
   key_name   = "digitrans-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSlIxRYXX9J7+JZ1VSSBESwCvBrjDQmTRsBzaJ92B0XzrSrGetdxnr8bQyom5aOtTWo6s3vHeg7lhakAUszUu51IZNEKE2cIDoBGyBqW8rxpce8k+4XW5PHSP3PauzInukloHBvbNOJsf/IrkLDLVMKJsnA7XhT5SpH4yraV2T8O7+4YnIJnEhm/Z2UrzEzZ+22Oetulp0gFS5xRDzklZaNpLTmg74Rt1+4G7ULHN0jMTQMCu0O4nITNceD1S2vaQeqaQKaA5HnuI66xvbKuDN8GYclBTA2AkHOcoF1dmMsIEigLUBuC7ZHEmYBP9Ar9MzFr4rrUda9U8jBFaZiKwI2cxDYtsvd/o3ZmTFWb38tZDzHw8odWJdiVS15Sh6ppS58rvnsGaSNBHy56JTwbqSedgmbNXYw085UneDWpc3GTTzZnTth3CWbBzrN82abXKe3JITNEiRaKKu2hiLNOc+UjbyjNVEDzXGKjpCSuKOuiGo09JyLz9JexcQrP6Ucs545ioByRB/C/wQkEFH5uTDY4tMbcKzDK3/wbo1dwfk0VBgF1R8gmOgwAw8qxSrP6uI3yEYX6fGDK2bRziCp84rr2lS6m0qOB7LbF8eI32lYv0c99PdXrponfdJl6p04NNiURz6ycjH9F+pEjiJGc2i8Htm1fAvh/gYesXR08sffw== userwitsel@WILFRIED"
 }
 
+# Nouveau nom unique pour éviter la collision sur AWS
 resource "aws_security_group" "digitrans_sg" {
-  name        = "digitrans-supply-sg"
+  name        = "digitrans-supply-chain-sg"
   description = "Access pour la stack DIGITRANS-CM"
 
   ingress {
