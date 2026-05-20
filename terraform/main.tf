da4c5c7f1e4f01d680359248b0034c0647852099
Woze Wilfried <wilfriedwoze@gmail.com>
2026-05-21 01:31:41 +0200

    fix: utilisation directe de la clé existante sur AWS

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Clé SSH gérée automatiquement
-resource "aws_key_pair" "digitrans_key" {
-  key_name   = "digitrans-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSlIxRYXX9J7+JZ1VSSBESwCvBrjDQmTRsBzaJ92B0XzrSrGetdxnr8bQyom5aOtTWo6s3vHeg7lhakAUszUu51IZNEKE2cIDoBGyBqW8rxpce8k+4XW5PHSP3PauzInukloHBvbNOJsf/IrkLDLVMKJsnA7XhT5SpH4yraV2T8O7+4YnIJnEhm/Z2UrzEzZ+22Oetulp0gFS5xRDzklZaNpLTmg74Rt1+4G7ULHN0jMTQMCu0O4nITNceD1S2vaQeqaQKaA5HnuI66xvbKuDN8GYclBTA2AkHOcoF1dmMsIEigLUBuC7ZHEmYBP9Ar9MzFr4rrUda9U8jBFaZiKwI2cxDYtsvd/o3ZmTFWb38tZDzHw8odWJdiVS15Sh6ppS58rvnsGaSNBHy56JTwbqSedgmbNXYw085UneDWpc3GTTzZnTth3CWbBzrN82abXKe3JITNEiRaKKu2hiLNOc+UjbyjNVEDzXGKjpCSuKOuiGo09JyLz9JexcQrP6Ucs545ioByRB/C/wQkEFH5uTDY4tMbcKzDK3/wbo1dwfk0VBgF1R8gmOgwAw8qxSrP6uI3yEYX6fGDK2bRziCp84rr2lS6m0qOB7LbF8eI32lYv0c99PdXrponfdJl6p04NNiURz6ycjH9F+pEjiJGc2i8Htm1fAvh/gYesXR08sffw== userwitsel@WILFRIED"
-}
-
-# Nouveau nom unique pour éviter la collision sur AWS
+# Nous utilisons un Security Group avec un nom unique et propre
 resource "aws_security_group" "digitrans_sg" {
   name        = "digitrans-supply-chain-sg"
   description = "Access pour la stack DIGITRANS-CM"
@@ -62,10 +56,11 @@ resource "aws_security_group" "digitrans_sg" {
   }
 }
 
+# L'instance pointe directement sur le nom de la clé 'digitrans-key' déjà présente sur AWS
 resource "aws_instance" "digitrans_ec2" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t3.medium"
-  key_name      = aws_key_pair.digitrans_key.key_name
+  key_name      = "digitrans-key"
 
   vpc_security_group_ids = [aws_security_group.digitrans_sg.id]
 
