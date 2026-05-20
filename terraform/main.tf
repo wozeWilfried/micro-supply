c15afa3f9add27773ea559ff394c6c98f8d40cc9
Woze Wilfried <wilfriedwoze@gmail.com>
2026-05-21 01:33:34 +0200

    fix: utilisation du bloc data pour recuperer le SG existant

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,58 +11,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Nous utilisons un Security Group avec un nom unique et propre
-resource "aws_security_group" "digitrans_sg" {
-  name        = "digitrans-supply-chain-sg"
-  description = "Access pour la stack DIGITRANS-CM"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP Frontend"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "API Backend"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "RabbitMQ Management"
-    from_port   = 15672
-    to_port     = 15672
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# On récupère le groupe de sécurité qui existe déjà sur ton compte AWS
+data "aws_security_group" "existing_sg" {
+  name = "digitrans-supply-chain-sg"
 }
 
-# L'instance pointe directement sur le nom de la clé 'digitrans-key' déjà présente sur AWS
+# Déploiement de l'instance EC2
 resource "aws_instance" "digitrans_ec2" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t3.medium"
-  key_name      = "digitrans-key"
+  key_name      = "digitrans-key" # Utilise la clé déjà présente sur AWS
 
-  vpc_security_group_ids = [aws_security_group.digitrans_sg.id]
+  # On associe l'ID du groupe de sécurité récupéré ci-dessus
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
   tags = {
     Name = "digitrans-supply-chain-server"
